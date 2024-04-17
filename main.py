dcc1cd8ebd981cc1c02d8737fcf06461259f15c7
Daniel-Duque <daniel_duque@javeriana.edu.co>
2024-04-17 09:06:45 -0500

    first demo

--- main.py
+++ main.py
@@ -5,4 +5,56 @@ Created on Tue Apr 16 11:06:54 2024
 
 @author: dduque
 """
+import pandas as pd
+import seaborn
+import matplotlib.pyplot as plt
+import streamlit as st
+
+comentario="""
+df=pd.read_excel(r"data/intermediate_income_calulations.xlsx")
+
+df["hhsize"]=df["hhsize"].astype(str)
+df=df[df["HHincome"]<1e7]
+df=df[df["HHincome"]>1]
+#some nice graphs that we won´t use
+#a code to make subsets of data
+
+g = seaborn.JointGrid(df,x="HHincome", y="hhsize",
+                 #cbar=True, 
+                 #legend=True
+                 )
+
+g.plot_joint(seaborn.histplot,hue_order=["1","2","3","4","5"])
+g.plot_marginals(seaborn.violinplot,inner="quart")
+seaborn.violinplot(df,x="HHincome",hue="hhsize")
+
+for numero_personas in range(1,19):
+    
+    df1=df[df["hhsize"]==str(numero_personas)]
+    df1.to_csv(r"data/"+str(numero_personas)+"sub.csv")
+
+"""
+st.title("Ingresos de hogares")
+
+tab0,tab1 = st.tabs(["Tu Ingreso","algo"])
+with tab0:
+    
+    numero_personas = st.selectbox("personas en tu hogar",
+       [i for i in range(1,17)])
+    
+    df1=pd.read_csv(r"data/"+str(numero_personas)+"sub.csv")
+    ingreso = st.number_input('Insert a number')
+    
+    fig, ax = plt.subplots()
+    seaborn.violinplot(ax=ax, data=df1,x="HHincome",hue="hhsize")
+    ax.axvline(ingreso)
+    st.pyplot(fig)
+
+
+
+
+
+
+
+
 
